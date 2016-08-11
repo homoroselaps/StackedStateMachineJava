488c07e8a801bb2af6e77ca76a01284801f50cf8
Jan-Tobias Matysik <jan-tobias.matysik@student.hpi.uni-potsdam.de>
2016-08-11 16:24:44 +0200

    fix

--- src/stackedStateMachine/DoneEvent.java
+++ src/stackedStateMachine/DoneEvent.java
@@ -2,5 +2,5 @@ package stackedStateMachine;
 
 public abstract class DoneEvent extends Event { }
 
-class SuccessEvent extends Event { }
-class FailureEvent extends Event { }
+class SuccessEvent extends DoneEvent { }
+class FailureEvent extends DoneEvent { }
